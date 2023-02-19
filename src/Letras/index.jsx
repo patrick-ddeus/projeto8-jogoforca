df7fc3c9072ee8db010e50235090e8a843e7500e
PatKatXD <100157955+PatKat20@users.noreply.github.com>
2023-02-18 23:20:07 -0300

    Cria componente Letras

---
+++ src/Letras/index.jsx
@@ -0,0 +1,26 @@
+import Button from '../button';
+
+const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function Letras ({palavra, setPalavraArray, palavraArray, contadorDeErros, setcontadorDeErros, resultado, setResultado}) {
+    return (
+        <div>
+            {alfabeto.map(letra => (
+                <Button 
+                value={letra} 
+                key={letra} 
+                palavra={palavra} 
+                palavraArray={palavraArray} 
+                setPalavraArray={setPalavraArray}
+                contadorDeErros={contadorDeErros}
+                setcontadorDeErros={setcontadorDeErros}
+                setResultado={setResultado}
+                inGame={resultado === "lose" || resultado === "win" ? false : true}
+                />
+                
+            ))}
+        </div>
+    );
+}
+
+export default Letras;
